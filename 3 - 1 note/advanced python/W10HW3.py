a20a9b7ae719933243cf91f34f813f7158b30245
dapin <89961651+dapin1490@users.noreply.github.com>
2022-05-09 12:35:18 +0900

    Update W10HW3.py

--- 3 - 1 note/advanced python/W10HW3.py
+++ 3 - 1 note/advanced python/W10HW3.py
@@ -8,6 +8,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # (1) 정수별로 카운트를 하는 함수
 def int_count(in_list):
     int_range = list(set(in_list))  # set으로 중복 제거 후 list로 만듦
@@ -22,15 +23,18 @@ def int_count(in_list):
     
     return int_range, count_list
 
+
 # (2) 각 정수별로 카운트된 값을 바 형식으로 출력하는 그림을 png 형태로 저장하는 함수
 # 그래프의 제목과 축 이름을 한글로 쓰면 그림에 네모 형태로밖에 나오지 않아 영어로 썼습니다.
-def save_count_bar(input_list):
-    x, y = int_count(input_list)
+def save_count_bar(x, y):
     plt.bar(x, y)
     plt.title("count each integer in bar format", fontdict={"fontsize":15})
     plt.xlabel("integer that exists in the list", fontdict={"fontsize":10})
     plt.ylabel("the number of each integer", fontdict={"fontsize":10})
     plt.savefig('count_integer_bar.png')
+    return
+
 
 input_list = [0, 3, 4, 4, 4, 4, 3, 3, 3, 3, 4, 4, 3, 3, 4, 4, 4, 4, 4, 4, 4, 2, 4, 4, 2, 4, 2, 4, 4, 3, 4, 3, 4, 4, 4, 4, 4, 4, 4, 4, 3, 4, 3, 4, 4, 1, 3, 4, 4, 2, 4, 2, 2, 4, 4, 2, 4, 3, 4, 4, 2, 4, 4, 2, 4, 4, 3, 4, 3, 4, 2, 4, 0, 3, 1, 4, 4, 4, 4, 4, 4, 4, 2, 4, 4, 3, 4, 4, 4, 4, 3, 4, 4, 3, 2, 3, 4, 4, 4, 3]
-save_count_bar(input_list)
+int_range, count_list = int_count(input_list)
+save_count_bar(int_range, count_list)
