0d83e606442c1cf4766632a44fc8b8bee3372ecd
dapin <89961651+dapin1490@users.noreply.github.com>
2022-05-12 17:26:26 +0900

    Update H_cpp_W9HW12-2.cpp

--- 3 - 1 note/advanced cpp/W9/H_cpp_W9HW12-2.cpp
+++ 3 - 1 note/advanced cpp/W9/H_cpp_W9HW12-2.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string> // C++ 문자열 string
 #include <cstring> // C 문자열 char[], strcmp()
@@ -15,12 +16,12 @@ class Person {
 private:
 	char id[MAX_ID];
 	string pw; // int로 저장하면 앞자리가 0으로 시작할 경우 "4자리" 유지가 안 됨
-	string birth; // 비밀번호와 같은 사유
+	int birth;// 생년은 제대로 입력한다면 0으로 시작할 일이 없다
 	double height;
 	int math, eng;
 
 public:
-	Person(const char* i = "", string p = "0000", string b = "0000", double h = 0.0, int m = 0, int e = 0) {
+	Person(const char* i = "", string p = "0000", int b = 0000, double h = 0.0, int m = 0, int e = 0) {
 		memcpy(id, i, MAX_ID);
 		pw = p;
 		birth = b;
@@ -31,7 +32,7 @@ public:
 	char* getId() { return id; }
 	string getPw() { return pw; }
 	
-	string getBirth() { return birth; }
+	int getBirth() { return birth; }
 	double getHeight() { return height; }
 	
 	int getMath() { return math; }
@@ -58,13 +59,14 @@ int main()
 	Person ps[5];
 	int n = 5;
 
-	ps[0] = Person("dnfn", "1234", "2000", 155.7, 91, 90);
-	ps[1] = Person("asdf", "0001", "2002", 159.3, 95, 91);
-	ps[2] = Person("bluesky", "1004", "2001", 165.0, 96, 92);
-	ps[3] = Person("seoul", "4321", "2000", 162.3, 93, 98);
-	ps[4] = Person("sswu21", "8990", "2002", 167.3, 97, 88);
+	ps[0] = Person("dnfn", "1234", 2000, 155.7, 91, 90);
+	ps[1] = Person("asdf", "0001", 2002, 159.3, 95, 91);
+	ps[2] = Person("bluesky", "1004", 2001, 165.0, 96, 92);
+	ps[3] = Person("seoul", "4321", 2000, 162.3, 93, 98);
+	ps[4] = Person("sswu21", "8990", 2002, 167.3, 97, 88);
 
 	// (2) 1번을 void display_top3(Person *p, int n) (n은 사람 명수)를 만들어 구현하세요.
+	printf("\n(2) 1번을 void display_top3(Person *p, int n) (n은 사람 명수)를 만들어 구현하세요.\n");
 	display_top3(ps, n);
 
 	return 0;
