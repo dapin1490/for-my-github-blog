aad454556e38da02ca4de92b3478223a2a253316
dapin <89961651+dapin1490@users.noreply.github.com>
2022-05-13 11:43:17 +0900

    Update ds_W10HW13.cpp

--- 3 - 1 note/data structure/ds_W10HW13.cpp
+++ 3 - 1 note/data structure/ds_W10HW13.cpp
@@ -67,3 +67,84 @@ void main()
 
 	stack.display();
 }
+
+/*
+// 교수 답안
+#include "stdlib.h"
+#include <iostream>
+using namespace std;
+
+class Node {	
+	Node* link; 		// 다음 노드를 가리키는 포인터 변수
+	char data;
+public:
+	Node(char in){ // 클래스 이름이 Node인데, 함수 이름이 Node => 생성자(인스턴스 생성할 때, 호출되는 함수)
+		link = NULL;
+        data = in;
+	}
+	~Node(void) { } // ~Node() => 소멸자(Destructor) (인스턴스 삭제시 호출되는 함수) 
+	Node* getLink() { return link; }
+	void setLink(Node* p) { link = p; }
+    char getData() { return data; }
+    void setData(char in) { data = in; }
+};
+
+class LinkedStack {
+    Node* top;
+public:
+    LinkedStack() { top = NULL; }
+    ~LinkedStack() { while (!isEmpty()) delete pop(); }
+    bool isEmpty() { return top == NULL; }
+
+    void push(Node* p) {
+        if (isEmpty()){
+            top = p;
+        }
+        else {
+            p->setLink(top);		// p->link = top;
+            top = p;
+        }
+    }
+
+    Node* pop()
+    {
+        Node* p = NULL;
+        if (isEmpty()) {
+            cout << "stack이 비어 있어서 리턴할 데이터가 없다" << endl;
+        }
+        else
+        {
+            p = top;
+            top = p->getLink();//p->link; private하니까 get 함수로 접근
+        }
+        return p;
+    }
+
+    // display
+    void display()
+    {
+	    Node *p;
+
+        for (Node* p = top; p != NULL; p = p->getLink())
+        {
+            cout << p->getData() << " ";
+        }
+        cout << endl;
+
+
+    }
+};
+
+void main()
+{
+    LinkedStack s1;
+    Node* return_data;
+    s1.push(new Node('{'));
+    s1.push(new Node('['));
+    s1.push(new Node('('));
+    s1.display();
+    return_data = s1.pop();
+    cout << return_data->getData() << endl;
+    s1.display();
+}
+*/
